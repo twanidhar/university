1d982fe7fe7107b93c501f3b687102122daf1905
Twani Dhar <twani.dhar@gmail.com>
2018-11-28 10:39:18 -0500

    group project

---
+++ src/test/java/co/grandcircus/University/ApplicationTests.java
@@ -0,0 +1,16 @@
+package co.grandcircus.University;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
