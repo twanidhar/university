80573267a89935ac5e33ba176050a2af15ec5f69
Twani Dhar <twani.dhar@gmail.com>
2018-11-28 13:43:11 -0500

    i don't know

--- src/main/java/co/grandcircus/University/AdminController.java
+++ src/main/java/co/grandcircus/University/AdminController.java
@@ -1,5 +1,23 @@
 package co.grandcircus.University;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller 
 public class AdminController {
+	
+	@Autowired
+	//TODO Dao dao;
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		return new ModelAndView("index");
+	}
+	
+	
 
 }
