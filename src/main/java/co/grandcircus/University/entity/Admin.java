8d344c2c0b3558e7512c7634b13fa20428132f10
Jacob Miller <miller.jacob16@gmail.com>
2018-11-28 11:31:12 -0500

    added admin

--- src/main/java/co/grandcircus/University/entity/Admin.java
+++ src/main/java/co/grandcircus/University/entity/Admin.java
@@ -1,5 +1,15 @@
 package co.grandcircus.University.entity;
 
-public class Admin {
+public class Admin extends User {
+
+	public Admin() {
+		super();
+	}
+
+	public Admin(Long id, String first, String last, String email, String password) {
+		super(id, first, last, email, password);
+	}
+	
+	
 
 }
